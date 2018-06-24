d2899b8d0157a481f18622bd72afc6c4210b372b
Rahul <rahulseh1998@gmail.com>
2018-06-24 10:37:51 +0530

    Add files via upload

---
+++ FinaltestApplication.java
@@ -0,0 +1,15 @@
+package com.demo.springboot.finaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaltestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FinaltestApplication.class, args);
+
+
+    }
+}
